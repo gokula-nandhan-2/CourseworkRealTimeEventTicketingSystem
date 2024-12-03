7565e0d02d253ffe5078d990d50e45c1e9483699
Gokula Nandhan <gokula.20233027@iit.ac.lk>
2024-12-03 13:43:34 +0530

    Adding files
    Adding spring boot files to repository

---
+++ src/main/java/coursework/oop/RealTimeEventTicketingSystem/RealTimeEventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package coursework.oop.RealTimeEventTicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coursework/oop/RealTimeEventTicketingSystem/RealTimeEventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework.oop.RealTimeEventTicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeEventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
